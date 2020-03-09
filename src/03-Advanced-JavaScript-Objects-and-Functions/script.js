63a9e6b794ebd9d1e8d76b1a9d87b30e26597100
unknown <darzi.reza@gmail.com>
2020-03-09 13:04:11 -0700

    Everything is an OBJECT

---
+++ src/03-Advanced-JavaScript-Objects-and-Functions/script.js
@@ -0,0 +1,5 @@
+// Everything is an OBJECT
+
+// Primitives -> Numbers, Strings, Booleans, Undefined, Null
+// Objects -> Array, Functions, Objects, Dates, Wrappers of Numbers, Strings aanndd ..........
+
