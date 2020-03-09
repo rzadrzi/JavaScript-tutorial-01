ffba7fe4b7bf213519bb124ce72606d261b5c705
unknown <darzi.reza@gmail.com>
2020-03-09 13:19:18 -0700

    Function Constructors

--- src/03-Advanced-JavaScript-Objects-and-Functions/script.js
+++ src/03-Advanced-JavaScript-Objects-and-Functions/script.js
@@ -3,3 +3,14 @@
 // Primitives -> Numbers, Strings, Booleans, Undefined, Null
 // Objects -> Array, Functions, Objects, Dates, Wrappers of Numbers, Strings aanndd ..........
 
+
+var Person = function(name, yearOfBirthday, job){ // Person is constructor function
+    this.name = name;
+    this.yearOfBirthday = yearOfBirthday;
+    this.job = job;
+}
+
+var reza = new Person('reza', 1987, 'Digital Marketing');
+
+console.log(reza);
+
