8930551f30d7c1849ffc07f9d3bd5d9711beeaa6
unknown <darzi.reza@gmail.com>
2020-03-09 12:18:21 -0700

    create a boolean variable for ending playing and it's name is gamePlaying

--- src/02-js-Browser-DOM-Manipulation-and-Events/app.js
+++ src/02-js-Browser-DOM-Manipulation-and-Events/app.js
@@ -40,42 +40,46 @@ function init(){
 }
 
 document.querySelector('.btn-roll').addEventListener('click',function(e){
-    
-    // 1. Random number
-    dice = Math.floor(Math.random() * 6) + 1;
+    if(gamePlaying){
+        // 1. Random number
+        dice = Math.floor(Math.random() * 6) + 1;
 
-    // 2. Display the result
-    var diceDOM = document.querySelector('.dice');
-    diceDOM.style.display = 'block';
-    diceDOM.src=`dice-${dice}.png`;
+        // 2. Display the result
+        var diceDOM = document.querySelector('.dice');
+        diceDOM.style.display = 'block';
+        diceDOM.src=`dice-${dice}.png`;
 
-    // 3. Update the round score IF the rolled was NOT a 1
-    if(dice !== 1){
-        // Add score
-        roundScore += dice;
-        document.querySelector(`#current-${activePlayer}`).textContent = roundScore;
-    }else{
-        // Next player
-        nextPlayer()
+        // 3. Update the round score IF the rolled was NOT a 1
+        if(dice !== 1){
+            // Add score
+            roundScore += dice;
+            document.querySelector(`#current-${activePlayer}`).textContent = roundScore;
+        }else{
+            // Next player
+            nextPlayer()
+        }
     }
 });
 
 document.querySelector('.btn-hold').addEventListener('click', function(e){
-    // Add CURRENT score to SCORES 
-    scores[activePlayer] += roundScore;
+    if(gamePlaying){
+        // Add CURRENT score to SCORES 
+        scores[activePlayer] += roundScore;
 
-    // Update UI
-    document.querySelector(`#score-${activePlayer}`).textContent = scores[activePlayer];
+        // Update UI
+        document.querySelector(`#score-${activePlayer}`).textContent = scores[activePlayer];
 
-    // Check if player WON game
-    if(scores[activePlayer] >= 10){
-        document.querySelector(`#name-${activePlayer}`).textContent = 'Winner';
-        document.querySelector('.dice').style.display = 'none';
-        document.querySelector(`.player-${activePlayer}-panel`).classList.add('winner');
-        document.querySelector(`.player-${activePlayer}-panel`).classList.remove('active');
-    }else{
-        // Next player
-        nextPlayer();
+        // Check if player WON game
+        if(scores[activePlayer] >= 10){
+            document.querySelector(`#name-${activePlayer}`).textContent = 'Winner';
+            document.querySelector('.dice').style.display = 'none';
+            document.querySelector(`.player-${activePlayer}-panel`).classList.add('winner');
+            document.querySelector(`.player-${activePlayer}-panel`).classList.remove('active');
+            gamePlaying = false;
+        }else{
+            // Next player
+            nextPlayer();
+        }
     }
  
 });
